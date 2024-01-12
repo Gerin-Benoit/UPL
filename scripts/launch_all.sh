8c08f992768f9bba3744744be9fdd540b1dd5f3d
Gerin-Benoit <benoit.gerin@uclouvain.be>
2024-01-12 10:30:39 +0100

    fix imagenet

--- scripts/launch_all.sh
+++ scripts/launch_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-datasets=("sscaltech101" "ssdtd" "sseurosat" "ssfgvc_aircraft" "ssfood101" "ssimagenet" "ssoxford_flowers" "ssoxford_pets" "ssstanford_cars" "sssun397" "ssucf101")
+# datasets=("sscaltech101" "ssdtd" "sseurosat" "ssfgvc_aircraft" "ssfood101" "ssimagenet" "ssoxford_flowers" "ssoxford_pets" "ssstanford_cars" "sssun397" "ssucf101")
+datasets=("ssimagenet")
 
 for dataset in "${datasets[@]}"; do
   CUDA_VISIBLE_DEVICES=0 bash get_info.sh "$dataset" anay_rn50 end 16 -1 False
