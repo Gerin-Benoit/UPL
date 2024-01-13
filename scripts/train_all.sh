c4058f6da619d868c1b20ec0efd515d0e1f25a99
Benoit <benoit.gerin@uclouvain.be>
2024-01-13 13:26:35 +0100

    test fix for imagenet

--- scripts/train_all.sh
+++ scripts/train_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-datasets=("sscaltech101" "ssdtd" "sseurosat" "ssfgvc_aircraft" "ssfood101" "ssimagenet" "ssoxford_flowers" "ssoxford_pets" "ssstanford_cars" "sssun397" "ssucf101")
+datasets=("sscaltech101" "ssdtd" "sseurosat" "ssfgvc_aircraft" "ssfood101" "ssoxford_flowers" "ssoxford_pets" "ssstanford_cars" "sssun397" "ssucf101" "ssimagenet")
 
 for dataset in "${datasets[@]}"; do
   CUDA_VISIBLE_DEVICES=0 bash upl_train.sh "$dataset" rn50_ep50 end 16 16 False True rn50_random_init > ~/train_logs.txt
