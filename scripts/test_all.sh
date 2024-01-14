6858ca5757968a0457c69a17c7d5c964765f5da9
Benoit <benoit.gerin@uclouvain.be>
2024-01-14 11:34:20 +0100

    test script

---
+++ scripts/test_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+datasets=("sscaltech101" "ssdtd" "sseurosat" "ssfgvc_aircraft" "ssfood101" "ssoxford_flowers" "ssoxford_pets" "ssstanford_cars" "sssun397" "ssucf101" "ssimagenet-a" "ssimagenet-r" "ssimagenet-sketch" "ssimagenet-v2" "ssimagenet")
+
+for dataset in "${datasets[@]}"; do
+  bash upl_test_existing_logits.sh "$dataset" rn50_ep50 end 16 16 False True >> ~/eval_logs.txt
+done
