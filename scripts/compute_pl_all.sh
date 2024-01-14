887f36b244f3d03ffac15349be4d6e8a7f96a7aa
Benoit <benoit.gerin@uclouvain.be>
2024-01-14 12:06:34 +0100

    test script

--- scripts/compute_pl_all.sh
+++ scripts/compute_pl_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#datasets=("sscaltech101" "ssdtd" "sseurosat" "ssfgvc_aircraft" "ssfood101" "ssoxford_flowers" "ssoxford_pets" "ssstanford_cars" "sssun397" "ssucf101")
-datasets=("ssimagenet-a" "ssimagenet-sketch" "ssimagenet-v2")
+datasets=("sscaltech101" "ssdtd" "sseurosat" "ssfgvc_aircraft") # "ssfood101" "ssoxford_flowers" "ssoxford_pets" "ssstanford_cars" "sssun397" "ssucf101")
+#datasets=("ssimagenet-a" "ssimagenet-sketch" "ssimagenet-v2")
 
 for dataset in "${datasets[@]}"; do
-  CUDA_VISIBLE_DEVICES=0 bash get_info.sh "$dataset" anay_rn50 end 16 -1 False # > ~/getpl_logs.txt
+  CUDA_VISIBLE_DEVICES=0 bash get_info.sh "$dataset" anay_rn50 end 16 -1 False > ~/getpl_logs_$dataset.txt
 done
 
--- scripts/train_all.sh
+++ scripts/train_all.sh
@@ -3,9 +3,6 @@
 datasets=("sscaltech101" "ssdtd" "sseurosat" "ssfgvc_aircraft" "ssfood101" "ssoxford_flowers" "ssoxford_pets" "ssstanford_cars" "sssun397" "ssucf101" "ssimagenet-a" "ssimagenet-r" "ssimagenet-sketch" "ssimagenet-v2" "ssimagenet")
 
 for dataset in "${datasets[@]}"; do
-  CUDA_VISIBLE_DEVICES=0 bash upl_train.sh "$dataset" rn50_ep50 end 16 16 False True rn50_random_init > ~/train_logs.txt
+  CUDA_VISIBLE_DEVICES=0 bash upl_train.sh "$dataset" rn50_ep50 end 16 16 False True rn50_random_init > ~/train_logs_$dataset.txt
 done
 
-for dataset in "${datasets[@]}"; do
-  bash upl_test_existing_logits.sh "$dataset" rn50_ep50 end 16 16 False True > ~/eval_logs.txt
-done
