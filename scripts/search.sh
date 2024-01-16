f6461b2e12723274cce545c9d06112c80a06ba39
Benoit <benoit.gerin@uclouvain.be>
2024-01-16 14:53:54 +0100

    coopupl first test

---
+++ scripts/search.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+datasets=("ssdtd" "sseurosat")
+shots=(1 2 4 8 16)
+lss=(0.1 0.3 0.5 0.7 0.9 1 1.5 2)
+
+
+#datasets=("ssimagenet")
+
+for dataset in "${datasets[@]}"; do
+  for shot in "${shots[@]}"; do
+    for ls in "${lss[@]}"; do
+      CUDA_VISIBLE_DEVICES=2 bash coopupl_train.sh "$dataset" rn50_ep50 end 16 "$shot" False True "$ls" 1
+      done
+  done
+done
