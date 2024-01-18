bb2d070a49821633dadde27c8e963d7574e993f4
Benoit <benoit.gerin@uclouvain.be>
2024-01-18 13:49:18 +0100

    fewshot ready

--- scripts/launch_fewshot_rn50.sh
+++ scripts/launch_fewshot_rn50.sh
@@ -8,7 +8,7 @@ seeds=(1 2 3)
 for dataset in "${datasets[@]}"; do
   for shot in "${shots[@]}"; do
     for seed in "${seeds[@]}"; do
-      CUDA_VISIBLE_DEVICES=1 bash coopupl_train_and_val.sh "$dataset" rn50_ep50 end 16 "$shot" False True resnet50_random_init "$seed" > ~/trainval_logs/rn50_"$dataset"_"$shot"shots_seed"$seed".txt
+      CUDA_VISIBLE_DEVICES=1 bash coopupl_train_and_val.sh "$dataset" rn50_ep50 end 16 "$shot" False True "$seed" > ~/trainval_logs/rn50_"$dataset"_"$shot"shots_seed"$seed".txt
     done
   done
 done
