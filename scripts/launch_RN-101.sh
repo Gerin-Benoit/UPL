47d54e74a5e1ba9997573b13dc70a872ccfa6dc6
Benoit <benoit.gerin@uclouvain.be>
2024-01-18 18:58:52 +0100

    rn101 zero shot ready

---
+++ scripts/launch_RN-101.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+datasets=("sscaltech101" "ssdtd" "sseurosat" "ssfgvc_aircraft" "ssfood101" "ssoxford_flowers" "ssoxford_pets" "ssstanford_cars" "sssun397" "ssucf101" "ssimagenet-a" "ssimagenet-r" "ssimagenet-sketch" "ssimagenet-v2" "ssimagenet")
+
+
+#for dataset in "${datasets[@]}"; do
+#  CUDA_VISIBLE_DEVICES=1 bash get_info.sh "$dataset" anay_ViT_B_16 end 16 -1 False
+#done
+
+for dataset in "${datasets[@]}"; do
+  CUDA_VISIBLE_DEVICES=1 bash upl_train.sh "$dataset" anay_rn101 end 16 16 False True rn101_random_init > ~/train_logs_rn101_$dataset.txt
+done
+
+
+
+
