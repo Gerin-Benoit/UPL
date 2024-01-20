7753e3c9350766e35ded94adac6a15e0e26c32a3
Benoit <benoit.gerin@uclouvain.be>
2024-01-20 21:02:57 +0100

    rn101 zero shot ready

---
+++ scripts/launch_fewshot_vitl14.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+datasets=("sscaltech101" "ssdtd" "sseurosat" "ssfgvc_aircraft" "ssfood101" "ssoxford_flowers" "ssoxford_pets" "ssstanford_cars" "sssun397" "ssucf101" "ssimagenet-a" "ssimagenet-r" "ssimagenet-sketch" "ssimagenet-v2" "ssimagenet")
+shots=(1 2 4 8 16)
+seeds=(1 2 3)
+
+
+for dataset in "${datasets[@]}"; do
+  for shot in "${shots[@]}"; do
+    for seed in "${seeds[@]}"; do
+      CUDA_VISIBLE_DEVICES=3 bash coopupl_train_and_val_vit.sh "$dataset" anay_ViT_L_14 end 16 "$shot" False True "$seed" > ~/trainval_logs/vitl14_"$dataset"_"$shot"shots_seed"$seed".txt
+    done
+  done
+done
+
