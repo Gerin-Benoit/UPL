9457e8d9ad8569b111bb1dc0d6e0f813badad94f
Gerin-Benoit <benoit.gerin@uclouvain.be>
2024-01-24 16:44:19 +0100

    TEMPLATES implem

---
+++ scripts/launch_all_TEMPLATES.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+datasets=("ssimagenet-a" "ssimagenet-r" "ssimagenet-sketch" "ssimagenet-v2")
+archs=("anay_rn50" "anay_ViT_B_16")
+
+for arch in "${archs[@]}"; do
+  for dataset in "${datasets[@]}"; do
+
+    bash upl_train_TEMPLATES.sh "$dataset" "$arch" end 16 16 False True "$arch"_random_init
+
+  done
+done
