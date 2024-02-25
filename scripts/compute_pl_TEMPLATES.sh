f7fb1ed6be19f8dcdf1d912762dd4611ee5b51fc
Gerin-Benoit <benoit.gerin@uclouvain.be>
2024-02-25 11:05:28 +0100

    TEMPLATES implem

--- scripts/compute_pl_TEMPLATES.sh
+++ scripts/compute_pl_TEMPLATES.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-datasets=("ssimagenet-a" "ssimagenet-r" "ssimagenet-sketch" "ssimagenet-v2")
+datasets=("ssimagenet-a" "ssimagenet-r" "ssimagenet-sketch" "ssimagenet-v2" "ssimagenet")
 
 
 for dataset in "${datasets[@]}"; do
-  CUDA_VISIBLE_DEVICES=3 bash get_info_TEMPLATES.sh "$dataset" rn50_ep50 end 16 -1 False
+  CUDA_VISIBLE_DEVICES=3 bash get_info_TEMPLATES.sh "$dataset" anay_rn101 end 16 -1 False
 done
 
 for dataset in "${datasets[@]}"; do
-  CUDA_VISIBLE_DEVICES=3 bash get_info_TEMPLATES.sh "$dataset" anay_ViT_B_16 end 16 -1 False
+  CUDA_VISIBLE_DEVICES=3 bash get_info_TEMPLATES.sh "$dataset" anay_ViT_B_32 end 16 -1 False
+done
+
+for dataset in "${datasets[@]}"; do
+  CUDA_VISIBLE_DEVICES=3 bash get_info_TEMPLATES.sh "$dataset" anay_ViT_L_14 end 16 -1 False
 done
 
 
