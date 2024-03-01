d106d240596031d16b58a76b54ec44166d1c3735
Gerin-Benoit <benoit.gerin@uclouvain.be>
2024-03-01 15:21:45 +0100

    for memory

--- trainers/upltrainer.py
+++ trainers/upltrainer.py
@@ -598,7 +598,7 @@ class UPLTrainer(TrainerX):
         predict_label_dict, _ = select_top_k_similarity_per_class(sstrain_outputs, sstrain_img_paths, -1, image_features, True)
 
 
-        save_outputs(self.train_loader_sstrain, self, predict_label_dict, self.cfg.DATASET.NAME, text_features, backbone_name=self.cfg.MODEL.BACKBONE.NAME)  # train_loader_x -> train_loader_sstrain
+        save_outputs(self.train_loader_sstrain, self, predict_label_dict, self.cfg.DATASET.NAME, text_features, backbone_name=self.cfg.MODEL.BACKBONE.NAME, specific_folder="./temp_analyze_results_miltiple/analyze_results")  # train_loader_x -> train_loader_sstrain
         caculate_noise_rate_analyze(predict_label_dict, train_loader=self.train_loader_sstrain, trainer=self)
         return predict_label_dict
 
@@ -639,7 +639,7 @@ class UPLTrainer(TrainerX):
                                                                   image_features, True)
 
         save_outputs(self.train_loader_sstrain, self, predict_label_dict, self.cfg.DATASET.NAME, text_features,
-                     backbone_name=self.cfg.MODEL.BACKBONE.NAME, specific_folder='analyze_results_TEMPLATES')  # train_loader_x -> train_loader_sstrain
+                     backbone_name=self.cfg.MODEL.BACKBONE.NAME, specific_folder="./temp_analyze_results_miltiple/analyze_results_TEMPLATES")  # train_loader_x -> train_loader_sstrain
         caculate_noise_rate_analyze(predict_label_dict, train_loader=self.train_loader_sstrain, trainer=self)
         return predict_label_dict
 
