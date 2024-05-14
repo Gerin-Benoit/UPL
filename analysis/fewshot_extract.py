d1655b474fbaffe6aa08a7934c47a3db5c730b4e
Gerin-Benoit <benoit.gerin@uclouvain.be>
2024-05-14 22:26:44 +0200

    upl with 32 pl

--- analysis/fewshot_extract.py
+++ analysis/fewshot_extract.py
@@ -59,7 +59,7 @@ def compute_mean_accuracies(folder_path):
 output_file_path = 'fewshot_mean_accuracies_summary.txt'
 
 # Usage
-folder_path = '~/trainval_logs'
+folder_path = '/gpfs/home/acad/ucl-elen/gerinb/trainval_logs'
 mean_accuracies = compute_mean_accuracies(folder_path)
 
 # Sort mean_accuracies by arch, dataset, and n_shots for printing
